27e05bd09b1a4c2166a6a61d25bedbe3afce1ec7
airy-sift <thanks.xap@gmail.com>
2021-05-22 09:06:25 +0900

    init

---
+++ lib/src/sample.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Ex extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RefreshIndicator(
+      child: SingleChildScrollView(
+
+      ),
+    );
+  }
+}
